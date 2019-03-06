a8bbc4afca50d1b4966a270da373ccc575b140ed
cbpark <cbpark@gmail.com>
2019-03-06 20:31:25 +0900

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,11 +1,15 @@
-{ mkDerivation, base, mwc-random, stdenv, transformers, vector }:
+{ mkDerivation, base, mwc-random, parallel, stdenv, transformers
+, vector
+}:
 mkDerivation {
   pname = "nmssm-lightscalar";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base mwc-random transformers vector ];
+  executableHaskellDepends = [
+    base mwc-random parallel transformers vector
+  ];
   description = "For studies on the light scalar in the NMSSM";
   license = stdenv.lib.licenses.gpl3;
 }
