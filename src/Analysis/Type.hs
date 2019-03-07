e04d71aa61a8ff1ecf57109cacc74585a2c8651a
cbpark <cbpark@gmail.com>
2019-03-07 15:32:26 +0900

    Add builders for the outputs of results

--- src/Analysis/Type.hs
+++ src/Analysis/Type.hs
@@ -1,4 +1,9 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Analysis.Type where
+
+import Data.ByteString.Builder
+import Data.Double.Conversion.ByteString (toExponential, toFixed)
 
 type Mass = Double
 type Coupling = Double
@@ -9,18 +14,38 @@ data TreeLevelCouplings = TreeLevelCouplings { cTop    :: !Double
                                              , cVector :: !Double
                                              } deriving Show
 
+renderTreeLevelCouplings :: TreeLevelCouplings -> Builder
+renderTreeLevelCouplings TreeLevelCouplings {..} =
+    convDbl cTop <> space
+    <> convDbl cBottom <> space
+    <> convDbl cVector
+
 data LoopLevelCouplings = LoopLevelCouplings { cGluon :: !Double
                                              , cGamma :: !Double
                                              } deriving Show
+
+renderLoopLevelCouplings :: LoopLevelCouplings -> Builder
+renderLoopLevelCouplings LoopLevelCouplings {..} =
+    convDbl cGluon <> space <> convDbl cGamma
 
 data HiggsCoupling = HiggsCoupling { tree :: !TreeLevelCouplings
                                    , loop :: !LoopLevelCouplings
                                    } deriving Show
 
+renderHiggsCoupling :: HiggsCoupling -> Builder
+renderHiggsCoupling HiggsCoupling {..} =
+    renderTreeLevelCouplings tree <> space <> renderLoopLevelCouplings loop
+
 data MixingAngles = MixingAngles { theta1 :: !Double
                                  , theta2 :: !Double
                                  , theta3 :: !Double
                                  } deriving Show
+
+renderMixingAngles :: MixingAngles -> Builder
+renderMixingAngles MixingAngles {..} =
+    convDbl theta1 <> space
+    <> convDbl theta2 <> space
+    <> convDbl theta3
 
 data NMSSMParameters = NMSSMParameters { lambda    :: !Double
                                        , tanbeta   :: !Double
@@ -28,8 +53,31 @@ data NMSSMParameters = NMSSMParameters { lambda    :: !Double
                                        , bigLambda :: !Double
                                        } deriving Show
 
-data NMSSMSolution = NMSSMSolution { params    :: !NMSSMParameters
-                                   , hCoupling :: !HiggsCoupling
-                                   , sCoupling :: !HiggsCoupling
-                                   , mixing    :: !MixingAngles
+renderNMSSMParameters :: NMSSMParameters -> Builder
+renderNMSSMParameters NMSSMParameters {..} =
+    (byteString . toFixed 2) lambda <> space
+    <> (byteString . toFixed 3) tanbeta <> space
+    <> convDbl mu <> space
+    <> convDbl bigLambda
+
+data NMSSMSolution = NMSSMSolution { params     :: !NMSSMParameters
+                                   , hCoupling  :: !HiggsCoupling
+                                   , sCoupling  :: !HiggsCoupling
+                                   , mixing     :: !MixingAngles
+                                   , muCMSValue :: !Double
+                                   , muLEPValue :: !Double
                                    } deriving Show
+
+renderNMSSMSolution :: NMSSMSolution -> Builder
+renderNMSSMSolution NMSSMSolution {..} =
+    renderNMSSMParameters params <> space
+    <> renderHiggsCoupling hCoupling <> space
+    <> renderHiggsCoupling sCoupling <> space
+    <> renderMixingAngles mixing <> space
+    <> convDbl muCMSValue <> space <> convDbl muLEPValue
+
+convDbl :: Double -> Builder
+convDbl = byteString . toExponential 4
+
+space :: Builder
+space = stringUtf8 "  "
--- src/searchnmssm.hs
+++ src/searchnmssm.hs
@@ -3,9 +3,12 @@ module Main where
 import           Analysis.EFT.Coupling
 import           Analysis.EFT.SignalStrength
 import           Analysis.NMSSM              (searchNMSSM)
+import           Analysis.Type
 import           Analysis.Util
 
 import           Control.Parallel.Strategies (using)
+import           Data.ByteString.Builder
+import qualified Data.ByteString.Lazy.Char8  as B
 import qualified Data.Vector                 as V
 import           System.Environment          (getArgs)
 
@@ -16,7 +19,6 @@ main = do
     putStrLn $ "mu_VV(h) = "   ++ show (muVV couplingHSM)
     putStrLn $ "mu_bb(h) = "   ++ show (muBB couplingHSM)
     putStrLn $ "mu_gaga(h) = " ++ show (muGaGa couplingHSM)
-    print $ searchNMSSM 0.65 1.5 (0.01, -0.0001)
 
     n <- head <$> getArgs
     ts <- mkTheta12 (read n)
@@ -25,5 +27,11 @@ main = do
     let lam = 0.65; tanb = 2
         -- cs = mapMaybe (searchNMSSM lam tanb) ts
         cs = catMaybes (V.map (searchNMSSM lam tanb) ts `using` parVectorChunk 200)
-    -- V.mapM_ print cs
+    V.mapM_ print cs
     print $ length cs
+
+    B.putStrLn $ toLazyByteString $ renderMixingAngles (MixingAngles 0.1 0.1 (-0.1))
+
+    -- let Just sol = searchNMSSM 0.65 1.5 (0.01, -0.0001)
+    -- print sol
+    -- B.putStrLn $ toLazyByteString $ renderNMSSMSolution sol
